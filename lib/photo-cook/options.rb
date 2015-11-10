449fd19e569d70a7e4aa214b2be4b6a93bd2afea
Yaroslav Konoplov <yaroslav@inbox.com>
2015-11-11 01:32:34 +0200

    Bugfixes

--- lib/photo-cook/options.rb
+++ lib/photo-cook/options.rb
@@ -3,7 +3,7 @@ module PhotoCook
     def parse_and_check_options(options)
       crop  = options.kind_of?(Hash) ? !!options[:crop] : !!options
       ratio = options.kind_of?(Hash) ? options[:pixel_ratio].to_f : 0.0
-      ratio = PhotoCook.current_client_pixel_ratio unless valid_pixel_ratio?(ratio)
+      ratio = PhotoCook.current_client_pixel_ratio.to_f unless valid_pixel_ratio?(ratio)
       [crop, valid_pixel_ratio?(ratio) ? ratio : 1.0]
     end
 
