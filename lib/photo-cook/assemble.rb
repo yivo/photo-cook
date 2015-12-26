25088e99dcf0cdca86bdacdb6ffa02b16d89233a
Yaroslav Konoplov <yaroslav@inbox.com>
2015-12-26 13:27:37 +0200

    Update assembling

--- lib/photo-cook/assemble.rb
+++ lib/photo-cook/assemble.rb
@@ -3,32 +3,24 @@ module PhotoCook
 
     # Edit URI so it will point to PhotoCook::Middleware
     # NOTE: This method performs no validation
-    def assemble_uri(uri, width, height, options = {})
-      ('/' if uri.start_with?('/')) +
-        File.join(cache_dir, File.dirname(uri),
-                  assemble_command(width, height, options), File.basename(uri))
+    def assemble_uri(uri, width, height, pixel_ratio, crop)
+        result = File.join cache_dir, dirname_or_blank(uri),
+                           assemble_command(width, height, pixel_ratio, crop), File.basename(uri)
+        uri.start_with?('/') ? '/' + result : result
     end
 
     # Edit path so it will point to place where resized photo stored
     # NOTE: This method performs no validation
-    def assemble_store_path(path, width, height, options = {})
+    def assemble_store_path(path, width, height, pixel_ratio, crop)
       rootless = path.split(File.join(root, public_dir)).second
       File.join root, public_dir,
-                cache_dir, File.dirname(rootless),
-                assemble_command(width, height, options), File.basename(path)
+                cache_dir, dirname_or_blank(rootless),
+                assemble_command(width, height, pixel_ratio, crop), File.basename(path)
     end
 
-    def resize(uri, width, height, options = {})
-      # TODO Validate args
-      assemble_uri(uri, width, height, options)
-    end
-
-    def hresize(uri, width, options = {})
-      resize(uri, width, :auto, options)
-    end
-
-    def vresize(uri, height, options = {})
-      resize(uri, :auto, height, options)
+  private
+    def dirname_or_blank(path)
+      (dirname = File.dirname(path)) == '.' ? '' : dirname
     end
   end
   extend Assemble
