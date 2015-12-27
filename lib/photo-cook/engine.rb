112c0951b16d049fe127ccaf0a024052c61c59d1
Yaroslav Konoplov <yaroslav@inbox.com>
2015-12-27 13:39:50 +0200

    Update engine

--- lib/photo-cook/engine.rb
+++ lib/photo-cook/engine.rb
@@ -1,9 +1,5 @@
 module PhotoCook
   class Engine < ::Rails::Engine
-    config.before_initialize do
-      PhotoCook.root = Rails.root
-    end
-
     initializer :photo_cook_javascripts do |app|
       app.config.assets.paths << File.join(PhotoCook::Engine.root, 'app/assets/javascripts')
     end
